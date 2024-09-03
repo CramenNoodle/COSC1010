88801e1087e7e00ae5ee937247d120c21a17a037
Carmen Grajeda <carmengrajeda@westernwyoming.edu>
2024-09-03 08:46:11 -0700

    Add files via upload

---
+++ COSC1010-main/COSC1010-main/Sales-Prediction/main.py
@@ -0,0 +1,16 @@
+#
+# Carmen Grajeda
+# 09/01/2024
+# Sales Prediction Programming Project
+# COSC 1010 NT
+
+
+# Variables to hold the sales total and the profit
+annualprofitpercentage = 0.23
+# Get the amount of projected sales.
+projectedAmount = float(input("Enter the projected sales:"))
+# Calculate the projected profit.
+profit = projectedAmount * annualprofitpercentage
+# Display the profit.
+print('The profit is $', format(profit,',.2f'))
+# Print the projected profit.
