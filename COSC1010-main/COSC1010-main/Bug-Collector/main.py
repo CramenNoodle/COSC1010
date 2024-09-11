f8cd8c30d6dd21dbb857e44f969f19da495378b3
Carmen Grajeda <carmengrajeda@westernwyoming.edu>
2024-09-11 12:15:20 -0600

    Add files via upload

--- COSC1010-main/COSC1010-main/Bug-Collector/main.py
+++ COSC1010-main/COSC1010-main/Bug-Collector/main.py
@@ -1,11 +1,16 @@
 #
-# Name
-# Date
+# Carmen Grajeda        
+# 09/09/2024
 # Bug Collector Programming Project
-# COSC 2409 DNT
+# COSC 1010 NT
 #
 # Initialize variables for bugs and total number of bugs collected.
-
+total_bugs = 0
 # Get number of bugs collected each day using a for loop.
-
+for day in range (1, 6):
+    print('Enter the amount of bugs you collected on day', day)
+    bugs = int(input())
+    total_bugs += bugs      
 # Display the total number of bugs collected.
+print('The total amount of bugs collected is', total_bugs, 'bugs.')
+
