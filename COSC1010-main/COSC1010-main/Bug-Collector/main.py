4b9bded0fa45d487ed910f01c153e8e8cc224b4b
Carmen Grajeda <carmengrajeda@westernwyoming.edu>
2024-09-15 13:05:32 -0600

    Add files via upload

--- COSC1010-main/COSC1010-main/Bug-Collector/main.py
+++ COSC1010-main/COSC1010-main/Bug-Collector/main.py
@@ -4,16 +4,19 @@
 # Bug Collector Programming Project
 # COSC 1010 NT
 
-# Constant Variable
-num_bugs = 0 # The number of bugs collected each day. 
+# Constant Variable to represent the amount of days. 
+day = 5
+# Local variables 
+numBugs = 0
 # Initilized variable for bugs and total number of bugs collects.
 total = 0 
-# Determine the amount of bugs collected each day using a for loop.
+# Get the bugs collected for each day using a for loop.
 for day in range (1, 6):
     print('Enter the amount of bugs you collected on day', day)
     # Calculate the number of bugs collected each day. 
-    num_bugs = int(input()) 
-    total += num_bugs      
+    numBugs = int(input()) 
+    # Add number of bugs to the accumulator.
+    total += numBugs      
 # Display the total number of bugs collected.
 print('The total amount of bugs collected is: ', total, 'bugs.')
 
