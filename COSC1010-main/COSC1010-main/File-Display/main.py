29cf084af5747acc80d9a23c75416a06d152285d
Carmen Grajeda <carmengrajeda@westernwyoming.edu>
2024-10-19 19:30:07 -0700

    Add files via upload

--- COSC1010-main/COSC1010-main/File-Display/main.py
+++ COSC1010-main/COSC1010-main/File-Display/main.py
@@ -8,12 +8,8 @@
 # This program opens a file named numbers.txt 
 # and displays all of the numbers in the file.
 
-# Constant Variable
-myfile = 'number.txt' # The file to be opened to display all of the numbers
-# Local Variables
-number = 0 
 # Open the file. 
-myfile = open('number.txt', 'r')
+myfile = open('numbers.txt', 'r')
 
 # Read and display the file's contents.
 for line in myfile:
